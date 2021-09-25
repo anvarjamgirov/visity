de51642ff93c4a8e243df2fe2a27fadeef1aff49
anvar_thinkland <anvar@thinkland.uz>
2021-09-25 12:30:07 +0500

    added: Worker, Outlet and Visit models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Worker(models.Model):
+    first_name = models.CharField(
+        max_length=127
+    )
+    phone_number = models.CharField(
+        max_length=31,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.first_name
+
+
+class Outlet(models.Model):
+    name = models.CharField(
+        max_length=127
+    )
+    worker = models.ForeignKey(
+        Worker,
+        on_delete=models.CASCADE,
+        related_name='outlets'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Visit(models.Model):
+    date = models.DateTimeField(
+        default=timezone.now
+    )
+    coordinates = models.CharField(
+        max_length=63
+    )
+    outlet = models.ForeignKey(
+        Outlet,
+        on_delete=models.CASCADE,
+        related_name='visits'
+    )
+
+    def __str__(self):
+        return f"{self.outlet.worker} came to {self.outlet}({self.coordinates}) in {self.date}"
